d4878090ae2497db552972828672450e3866ff3f
Olek Basarab <barcelona_chelsea@ukr.net>
2020-09-10 20:19:23 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {HeroDetailComponent} from './hero-detail.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroDetailComponent
+  ],
+  imports: [
+    BrowserModule, FormsModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
